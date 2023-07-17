c10f7d3734be5b004fba083b0ff0d0ee3d26b5ce
Koki Fukuda <ko.fu.dev@gmail.com>
2023-07-17 16:24:11 +0900

    Use kuromoji analyzer for Elasticsearch

--- app/chewy/accounts_index.rb
+++ app/chewy/accounts_index.rb
@@ -30,14 +30,16 @@ class AccountsIndex < Chewy::Index
     analyzer: {
       # "The FOOING's bar" becomes "foo bar"
       natural: {
-        tokenizer: 'standard',
+        char_filter: %w(icu_normalizer),
+        tokenizer: 'kuromoji_tokenizer',
         filter: %w(
           lowercase
-          asciifolding
+          kuromoji_baseform
+          kuromoji_part_of_speech
           cjk_width
+          ja_stop
+          kuromoji_stemmer
           elision
-          english_possessive_stemmer
-          english_stop
           english_stemmer
         ),
       },
